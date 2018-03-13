0a5fb0eebf60b08401be2e9499ad8ed4c430783d
miketitus <mike@mike-titus.com>
2018-03-13 13:33:45 -0700

    remove ssh key, fix comments

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,6 @@
 # # # ec2.tf # # #
 
-# SSH keypair for debugging
-
-resource "aws_key_pair" "splice_demo" {
-  key_name   = "splice-demo-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTpWlB2GQxjuaZzdr5RvhyMLE0fIdXf61U3YnotT3L3RK16D9LnLsFpnM9NUmBIQ0uCg+dErp54l5htwm8Wb1qURRbWXYq9Yc9hlUDqIvW3/z3vdGkwb2BSUYvx/5mtGFQZZWorgtuT0r3vBW0KJG3Qu1TCJwyQ20ksCHFYy4jlT/kH+HjodeSK810WNm07+pc5mfnEsgMkL+XZOydZYUy9V27Q2b492IFG+uwfIVH+yJTvs2rlV/7gzT9mTK76kDTzub0X3jNDXZMP1lOIcIP9AszDQ6+RdKZ7GXv2a243LoeQQ6YJJbhAUvQDzktZDvEhcjUzNWYNvAhS9HfrfXD"
-}
-
-# Security Group for ec2 instances
+# Security Group for EC2 instances
 
 resource "aws_security_group" "splice_demo_ec2" {
   name = "Splice-Demo-EC2-SG"
@@ -38,9 +31,9 @@ resource "aws_instance" "splice_demo" {
   availability_zone           = "${lookup(var.ec2_availability_zones, count.index)}"
   count                       = "${var.ec2_instance_count}"
   instance_type               = "t2.micro"
-  key_name                    = "${aws_key_pair.splice_demo.key_name}"
   user_data                   = "${file("user_data.sh")}"
 
+  # references security group created above
   vpc_security_group_ids = [
     "${aws_security_group.splice_demo_ec2.id}",
   ]
