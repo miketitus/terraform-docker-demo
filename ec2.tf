4735f400b60ca1a5a49539c93f0d384e7f69987e
miketitus <mike@mike-titus.com>
2018-03-12 17:43:49 -0700

    first commit

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+# # # ec2.tf # # #
+
+resource "aws_key_pair" "splice_demo" {
+  key_name   = "splice-demo-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTpWlB2GQxjuaZzdr5RvhyMLE0fIdXf61U3YnotT3L3RK16D9LnLsFpnM9NUmBIQ0uCg+dErp54l5htwm8Wb1qURRbWXYq9Yc9hlUDqIvW3/z3vdGkwb2BSUYvx/5mtGFQZZWorgtuT0r3vBW0KJG3Qu1TCJwyQ20ksCHFYy4jlT/kH+HjodeSK810WNm07+pc5mfnEsgMkL+XZOydZYUy9V27Q2b492IFG+uwfIVH+yJTvs2rlV/7gzT9mTK76kDTzub0X3jNDXZMP1lOIcIP9AszDQ6+RdKZ7GXv2a243LoeQQ6YJJbhAUvQDzktZDvEhcjUzNWYNvAhS9HfrfXD"
+}
+
+# TODO security group
+
+resource "aws_instance" "splice_demo" {
+  ami                         = "${var.ec2_ami}"
+  associate_public_ip_address = "true"
+  availability_zone           = "${lookup(var.ec2_availability_zones, count.index)}"
+  count                       = 2
+  instance_type               = "t2.micro"
+  key_name                    = "${aws_key_pair.splice_demo.key_name}"
+
+  tags {
+    Name = "Splice Demo  ${count.index}"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,26 @@
+# # # vars.tf # # #
+
+variable "aws_access_key" {
+  description = "AWS access key, must pass on command line"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret access key, must pass on command line"
+}
+
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+variable "ec2_ami" {
+  description = "Ubuntu Server 16.04 LTS (HVM)"
+  default     = "ami-79873901"
+}
+
+variable "ec2_availability_zones" {
+  default = {
+    "0" = "us-west-1a"
+    "1" = "us-west-1b"
+    "2" = "us-west-1c"
+  }
+}
