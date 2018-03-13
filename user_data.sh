1d115663b8f7f2aec5b4fdfaf93d059dd87ea265
miketitus <mike@mike-titus.com>
2018-03-13 10:19:00 -0700

    complete docker

--- user_data.sh
+++ user_data.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
+# user data scripts automatically execute as root user, so sudo not required
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-cache policy docker-ce
-sudo docker pull nginx:latest
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+# in a production instance I would run "apt-get dist-upgrade -y" here,
+# but I've ommitted it for purposes of speeding up instance launch
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+docker pull nginx:latest
+docker run -d -p 80:80 --name nginx nginx
